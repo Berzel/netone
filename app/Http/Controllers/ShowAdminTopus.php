be3bc5b7071828f0bfce6d1a6ea63baf9f49dadc
Berzel Best <berzel@classifieds.co.zw>
2021-08-12 02:32:31 +0200

    Get a list of topups

---
+++ app/Http/Controllers/ShowAdminTopus.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Topup;
+use Illuminate\Http\Request;
+
+class ShowAdminTopus extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        return response()->json(Topup::paginate());
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\EcocashWebhookController;
 use App\Http\Controllers\RechargePinlessController;
+use App\Http\Controllers\ShowAdminTopus;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::prefix('v1')->group(function () {
+    Route::get('admin/topups', ShowAdminTopus::class)->name('admin.topups.index');
     Route::post('topups', RechargePinlessController::class)->name('airtime.topup');
     Route::any('ecocash-payments/{payment}/webhook', EcocashWebhookController::class)->name('ecocash.webhook');
 });
