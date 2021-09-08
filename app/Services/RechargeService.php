4ad172652777ad8ca96a62f449cf6cfb6df30f87
Berzel Best <berzel@classifieds.co.zw>
2021-09-08 04:58:37 +0200

    Remove ref code stuff

--- app/Services/RechargeService.php
+++ app/Services/RechargeService.php
@@ -38,8 +38,6 @@ class RechargeService {
      */
     public function recharge(RechargeCommand $command) : Topup
     {
-        $refCode = array_key_exists('referral_code', $command->data()) ? $command->data()['referral_code'] : null;
-
         DB::beginTransaction();
         $topup = Topup::create($command->data());
         $payment = $this->paymentsService->create(new CreatePaymentCommand($command->data()));
@@ -47,17 +45,8 @@ class RechargeService {
         $topup->update([
             'payment_id' => $payment->id,
             'payment_method' => get_class($payment),
-            'amount' => isset($refCode) ? 1.07 * $payment->amount : 1.05 * $payment->amount
+            'amount' => $payment->amount
         ]);
-
-        if (isset($refCode)) {
-            $referrer = WhatsappUser::whereReferralCode($command->get('referral_code'))->first();
-
-            Referral::create([
-                'topup_id' => $topup->id,
-                'referrer_id' => $referrer->id
-            ]);
-        }
 
         event(new TopupInitiated($topup));
         DB::commit();
